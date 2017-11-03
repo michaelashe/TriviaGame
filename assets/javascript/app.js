1eeda6935c3535bc8c5c5c689f855d4b60fe9cd7
Cherie Ashe <CherieAshe@cheries-mbp.attlocal.net>
2017-11-03 17:51:43 -0400

    Files Updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,93 +1,105 @@
-$(document).ready(function() {
-  number = 45;
-  var intervalId = [];
-  var correctAnswer = 0;
-  var wrongAnswer = 0;
-  var unanswered = 0;
+$("#start").on("click", function(){
+    game.start();
 
+})
 
-  confirm("Start Quiz?")
-      run();
+$(document).on('click', '#end', function() {
+    game.done();
+})
 
-  function run() {
-      intervalId = setInterval(decrement, 1000);
-  };
-
-  function decrement() {
-        number--;
-        $("#counter").html("<h2>" + number + "</h2>");
-          console.log(intervalId);
-          if (number === 0) {
-            clearInterval(intervalId);
-            alert("Time's Up!");
-            }
-
-          if (number === 0) {
-            $("#message").append("<h2>Correct Answers: " + correctAnswer + "</h2>");
-            $("#message").append("<h2>Incorrect Answers: " + wrongAnswer + "</h2>");
-            $("#message").append("<h2>Unanswered Questions: " + unanswered + "</h2>");
-          }  
-      };
 
   var questions = [
 
     {
-      question: "Which wrestler has held the World Heavyweight Championship 16 times?",
-      choices: ["Ric Flair", "Steve Austin","John Cena"],
-      answer:    "Ric Flair",
+       question: "Which wrestler has held the World Heavyweight Championship 16 times?",
+       choices: ["Ric Flair", "Steve Austin", "John Cena"],
+       correctAnswer: "Ric Flair",
+     },
+
+     {
+       question: "Which wrestler had the longest undefeated streak at Wrestlemania?",
+       choices: ["Shawn Michaels", "The Undertaker", "Bret Hart"],
+       correctAnswer: "The Undertaker",
+      },
+
+     {
+       question: "Rick and Scott Steiner were a tag team known as?",
+       choices: ["The Road Warriors", "The Steiner Brothers", "The Fabulous Freebirds"],
+       correctAnswer: "The Steiner Brothers", 
+      },
+
+     {
+       question: "Stone Cold Steve Austin's birth name is?",
+       choices: ["Steve Allen", "Steve Williams", "Steve Anderson"],
+       correctAnswer: "Steve Williams",
+     }
+   ];
+
+   var game = {
+      correct: 0,
+      incorrect: 0,
+      counter: 50,
+      countdown: function(){
+        game.counter--;
+        $("#counter").html(game.counter);
+        if(game.counter<=0){
+          console.log("Time Up")
+          game.done();
+        }
+      },
+  start: function(){
+    timer = setInterval(game.countdown,1000);
+    $("#subwrapper").prepend('<h2>Time Remaining: <span id="counter">50</span> Seconds</h2>');
+    $("#start").remove();
+      for(var i=0; i < questions.length; i++) {
+          $("#subwrapper").append("<h2>" + questions[i].question + "</h2>")
+        for(var j=0; j < questions[i].choices.length; j++) {
+          $("#subwrapper").append("<input type='radio'name='question-"+i+"' value='"+questions[i].choices[j]+"'>"+ "  " + questions[i].choices[j] + " ")
+        }
+      }
+      $("#subwrapper").append('<br><br><br><button id="end" type="button" class="btn btn-primary">Submit</button>');
     },
+    done: function(){
+      $.each($("input[name='question-0']:checked"), function() {
+        if($(this).val()==questions[0].correctAnswer) {
+          game.correct++;
+        } else {
+          game.incorrect++;
+        }
+      });
+      $.each($("input[name='question-1']:checked"), function() {
+        if($(this).val()==questions[1].correctAnswer){
+          game.correct++;
+        } else {
+          game.incorrect++;
+        }
+      });
+      $.each($("input[name='question-2']:checked"), function() {
+        if($(this).val()==questions[2].correctAnswer){
+          game.correct++;
+        } else {
+          game.incorrect++;
+        }
+      });
+      $.each($("input[name='question-3']:checked"), function() {
+        if($(this).val()==questions[3].correctAnswer){
+          game.correct++;
+        } else {
+          game.incorrect++;
+        }
+      });
 
-    {
-      question: "Which wrestler had the longest undefeated streak at Wrestlemania?",
-      choices: ["Shawn Michaels", "The Undertaker", "Bret Hart"],
-      answer:    "The Undertaker",
-     },
-
-    {
-      question: "Rick and Scott Steiner were a tag team known as?",
-      choices: ["The Road Warriors", "The Steiner Brothers", "The Fabulous Freebirds"],
-      answer:    "The Steiner Brothers", 
-     },
-
-    {
-      question: "Stone Cold Steve Austin's birth name is?",
-      choices: ["Steve Allen", "Steve Williams", "Steve Anderson"],
-      answer: "Steve Williams",
-    }
-  ];
+         this.result();
+        },
 
 
-for (var i = 0; i < questions.length; i++) {
-  // adding the text of the question to HTML
-   var questionText = document.createElement("p");
-   questionText.innerHTML = questions[i].question;
-  $("#quiz").append("<p>" + questions[i].question + "</p>");
+        result: function(){
+          clearInterval(timer);
+          $("#subwrapper h2").remove();
 
-    for (var j = 0; j < 3; j++) {
-    $("#quiz").append("<input type='radio' name='"+i+"' >" + questions[i].choices[j] + "</input>");
-
-    }
+          $("#subwrapper").html("<h2>All Done!</h2>");
+          $("#subwrapper").append("<h3>Correct Answers: "+this.correct+"</h3>");
+          $("#subwrapper").append("<h3>Incorrect Answers: "+this.incorrect+"</h3>");
+          $("#subwrapper").append("<h3>Unanswered: "+(questions.length-(this.incorrect+this.correct))+"</h3>");
+        }
 }
-//Create an answer key that we can compare to user input
-var answerKey = [questions[0].choices[0], questions[1].choices[1], questions[2].choices[1], questions[3].choices[2]];
-console.log(answerKey);
-
-//How do you log radio button inputs from user?
-
-
-//When the submit button is pressed, tally answers into correct or incorrect responses
-
-$("#submitButton").click(function() {
-
-//Testing Button
-  alert("You Pressed Me!");
-
-
-});
-
-
-console.log(questions[0].choices[0]);
-
-
-
-});
